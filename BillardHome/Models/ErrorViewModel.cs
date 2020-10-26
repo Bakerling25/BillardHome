21955f38bc716ddd73c364b2020dabee7fa37b2b
Bakerling25 <thom97m6@edu.sde.dk>
2020-10-26 13:14:52 +0100

    Har lavet lidt af projektet. Er startet i dag

---
+++ BillardHome/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BillardHome.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
